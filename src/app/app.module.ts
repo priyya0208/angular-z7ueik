8c798da7f735e4a5bb0f3067f332bb045112bcca
priyya0208 <priyya.0208@gmail.com>
2019-11-25 12:54:21 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { UserViewComponent } from './user-view/user-view.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, RegistrationComponent ],
+  declarations: [ AppComponent, HelloComponent, RegistrationComponent, UserViewComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
